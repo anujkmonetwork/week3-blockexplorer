5994c8187723e11fb708450286887dd326e333e4
anujkmonetwork <anuj.k@monet.work>
2023-04-20 17:48:01 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -21,16 +21,54 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [transactions, setTransactions] = useState([]);
+  const [block, setBlock] = useState({});
+  const [transactionReceipt, setTransactionReceipt] = useState({});
 
   useEffect(() => {
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      // console.log()
+      const newBlockNumber = await alchemy.core.getBlockNumber();
+      // console.log("blockNumber: ", blockNumber);
+      const newBlock = await alchemy.core.getBlock(newBlockNumber);
+      // setBlock(newBlock);
+      // const transactions = await alchemy.core.getBlockWithTransactions(blockNumber);
+      setTransactions(newBlock.transactions);
+      // console.log("block: ", block.transactions);
+      // const transactionReceipt = await alchemy.core.getTransactionReceipt(block.transactions[0]);
+      // settra
+      // console.log("Transaction: ", transactionReceipt);
+      setBlockNumber(newBlockNumber);
     }
 
     getBlockNumber();
-  });
+  }, []);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  const handleTransactionClick = async (transaction) => {
+      const transactionReceipt = await alchemy.core.getTransactionReceipt(transaction);
+      console.log(transactionReceipt);
+      alert(`
+      To: ${transactionReceipt.to}
+      From: ${transactionReceipt.from}
+      Block Number: ${transactionReceipt.blockNumber}
+      `);
+  }
+
+  const getTransactions = () => {
+    console.log(transactions);
+    return transactions.map(transaction => {
+      return <div onClick={() => handleTransactionClick(transaction)}>
+        {transaction}
+      </div>
+    });
+  }
+
+  return <div className="App">
+    Block Number: {blockNumber}
+    <div>
+      {getTransactions()}
+    </div>
+  </div>;
 }
 
 export default App;
